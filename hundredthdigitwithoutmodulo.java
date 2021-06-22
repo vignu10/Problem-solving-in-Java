7acec874b1756a1689550f969bf63b8cf64a1d7c
vignu10 <75877958+vignu10@users.noreply.github.com>
2021-06-22 18:03:45 +0530

    Update hundredthdigitwithoutmodulo.java

--- hundredthdigitwithoutmodulo.java
+++ hundredthdigitwithoutmodulo.java
@@ -1,1 +1,12 @@
-import java.util.Scanner;public class {public static void main(String[] args){Scanner sc=new Scanner(System.in);Num= sc.nextint()Int n= num/1000;res=num-(n*1000)System.out.println(res/100)}}
+import java.util.Scanner;
+public class hundredth
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+Num= sc.nextint()
+Int n= num/1000;
+res=num-(n*1000)
+System.out.println(res/100)
+}
+}
