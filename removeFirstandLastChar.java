175386a101b07e6ac24a59c84d33bfc3d31903a6
vignu10 <75877958+vignu10@users.noreply.github.com>
2021-07-01 15:01:57 +0530

    Update removeFirstandLastChar.java

--- removeFirstandLastChar.java
+++ removeFirstandLastChar.java
@@ -5,7 +5,7 @@ public class Hello {
 		//Your Code Here
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		n=str.length();
+		int n=str.length();
 		System.out.print(str.substring(1,n-1));
 
 	}
